c49f41bf2adbd0268bae94068dae193904cd5446
Jim-EM <jim.em@fullstackwebpro.co.uk>
2018-01-12 10:45:53 +0000

    Adds file validation.

--- LeafNodeXpaths.php
+++ LeafNodeXpaths.php
@@ -1,15 +1,18 @@
 <?php
-
-    $tree =  simplexml_load_file($argv[1]);
-    
-    recurseChildren($tree);
+    if (is_file($argv[1]) && is_readable($argv[1])) {
+        $tree =  simplexml_load_file($argv[1]);
+        recurseChildren($tree);
+    }
+    else {
+        echo 'Cannot access this file, it might not be a file!' . "\n";
+    }
 
     function recurseChildren($node, $pathString = '') {
         $pathString .= '/' . $node->getName();
         foreach ($node->children() as $child) {
             recurseChildren($child, $pathString);
         }
-       
+
         if ($node->count() === 0) {
             echo $pathString . "\n";
         }
